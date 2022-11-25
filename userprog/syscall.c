e11cd4b6405e852705df5c9318f7575cd8e955d3
rigyeonghong <ubuntu@ip-172-31-7-103.ap-northeast-2.compute.internal>
2022-11-25 05:57:53 +0000

    feat: syscall close 58/95 test success! :)

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -28,7 +28,9 @@ int exec (const char *file);
 // int read (int fd, void *buffer, unsigned size);
 int open (const char *file);
 int add_file_to_fd_table(struct file *file);
-// void close (int fd);
+struct file *fd_to_file(int fd);
+void remove_fd(int fd); 
+void close (int fd);
 
 /* System call.
  *
@@ -115,8 +117,9 @@ syscall_handler (struct intr_frame *f UNUSED) {
 		case SYS_OPEN:
 			f->R.rax = open(f->R.rdi);
 			break;
-		// case SYS_CLOSE:
-		// 	close(f->R.rdi);
+		case SYS_CLOSE:
+			close(f->R.rdi);
+			break;
 		break;
 	}
 
@@ -190,15 +193,8 @@ int
 add_file_to_fdt(struct file *file){
 	struct thread *cur = thread_current();
 	struct file **cur_fd_table = cur->fd_table;
-	//  cur->fdidx 2 이상으로 하거나
-	// cur_fd_table[0], cur_fd_table[1] 을 채워주거나
-	// int *stdin = 0; 
-	// int *stdout = 1;
-	// cur_fd_table[0] = stdin;
-	// cur_fd_table[1] = stdout;
 	for (int i = cur->fdidx; i < MAX_FD_NUM; i++){
 		if (cur_fd_table[i] == NULL){
-			// printf("+++++++++++%d+++++++++++\n",i);
 			cur_fd_table[i] = file;
 			cur->fdidx = i;
 			return cur->fdidx;
@@ -217,7 +213,6 @@ open (const char *file) {
 	}
 	
 	int fd = add_file_to_fdt(open_file);
-
 	if (fd == -1){ // fd table 가득 찼다면
 		file_close(open_file);
 	}
@@ -244,7 +239,33 @@ fork (const char *thread_name, struct intr_frame *f){
 	return process_fork(thread_name, f);
 }
 
-// void
-// close (int fd) {
-// 	syscall1 (SYS_CLOSE, fd);
-// }
+/* 현재 프로세스의 fd에 있는 file 반환 */
+struct file *
+fd_to_file(int fd){
+	struct thread *cur = thread_current();
+	struct file **cur_fd_table = cur->fd_table;
+	if(0 <= fd && fd < MAX_FD_NUM){
+		return cur_fd_table[fd];
+	}else{
+		return NULL;
+	}
+}
+
+void
+remove_fd(int fd){
+	struct thread *cur = thread_current();
+	struct file **cur_fd_table = cur->fd_table;
+	cur_fd_table[fd] = NULL;
+}
+
+void
+close (int fd) {
+	// fd를 file로 변경해서 file_close()인자로 넣기
+	struct file *file = fd_to_file(fd);
+	if(file == NULL){
+		return;
+	}
+	file_close(file);
+	// fdt 에서 지워주기
+	remove_fd(fd);
+}
