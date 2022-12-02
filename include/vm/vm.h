d6e5cd751e35bf2f1541c94fba1d328663816a68
Ubuntu <ubuntu@ip-172-31-7-94.ap-northeast-2.compute.internal>
2022-12-02 05:48:50 +0000

    start

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -2,6 +2,8 @@
 #define VM_VM_H
 #include <stdbool.h>
 #include "threads/palloc.h"
+#include "include/lib/kernel/hash.h"
+
 
 enum vm_type {
 	/* page not initialized */
@@ -85,6 +87,7 @@ struct page_operations {
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
+	struct hash pages; // heesan
 };
 
 #include "threads/thread.h"
