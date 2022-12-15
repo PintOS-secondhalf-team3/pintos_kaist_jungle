6e1320eefe52a87be433779589b2aa050ffb4638
Ubuntu <ubuntu@ip-172-31-7-94.ap-northeast-2.compute.internal>
2022-12-15 02:34:26 +0000

    fat 관련 함수 주석 추가

--- filesys/fat.c
+++ filesys/fat.c
@@ -17,13 +17,13 @@ struct fat_boot {
 };
 
 /* FAT FS */
-// FAT 파일 시스템 정보를 담고 있음
+// 한 파일 내에서 사용하는 FAT 라고 추정..
 struct fat_fs {
-	struct fat_boot bs;
+	struct fat_boot bs; // 전체 파일 시스템의 정보가 담겨있는 듯?
 	unsigned int *fat;
-	unsigned int fat_length;
-	disk_sector_t data_start;
-	cluster_t last_clst;
+	unsigned int fat_length; // FS안에 들어가있는 sector의 갯수
+	disk_sector_t data_start; // 비어있는 첫 sector?
+	cluster_t last_clst; // 파일이 할당받은 cluster중, 마지막 cluster?
 	struct lock write_lock;
 };
 
@@ -34,6 +34,8 @@ void fat_fs_init (void);
 
 void
 fat_init (void) {
+	// fat_fs는 메모리 상에 올라가 있다. 
+	// 따라서 calloc (임의 사이즈를 여러번 malloc 받을 수 있도록) 을 받는다.
 	fat_fs = calloc (1, sizeof (struct fat_fs));
 	if (fat_fs == NULL)
 		PANIC ("FAT init failed");
@@ -42,14 +44,16 @@ fat_init (void) {
 	unsigned int *bounce = malloc (DISK_SECTOR_SIZE);
 	if (bounce == NULL)
 		PANIC ("FAT init failed");
+	
+	// FAT_BOOT_SECTOR 있는 내용을 bounce로 옮긴다.
 	disk_read (filesys_disk, FAT_BOOT_SECTOR, bounce);
 	memcpy (&fat_fs->bs, bounce, sizeof (fat_fs->bs));
 	free (bounce);
 
 	// Extract FAT info
 	if (fat_fs->bs.magic != FAT_MAGIC)
-		fat_boot_create ();
-	fat_fs_init ();
+		fat_boot_create (); // fat_fs -> bs 를 만들어 줌
+	fat_fs_init (); // fat_fs 를 만듬
 }
 
 void
@@ -59,10 +63,10 @@ fat_open (void) {
 		PANIC ("FAT load failed");
 
 	// Load FAT directly from the disk
-	uint8_t *buffer = (uint8_t *) fat_fs->fat;
+	uint8_t *buffer = (uint8_t *) fat_fs->fat; // buffer와 fat을 일치시킴
 	off_t bytes_read = 0;
 	off_t bytes_left = sizeof (fat_fs->fat);
-	const off_t fat_size_in_bytes = fat_fs->fat_length * sizeof (cluster_t);
+	const off_t fat_size_in_bytes = fat_fs->fat_length * sizeof (cluster_t); // 말 그대로 fat의 크기
 	for (unsigned i = 0; i < fat_fs->bs.fat_sectors; i++) {
 		bytes_left = fat_size_in_bytes - bytes_read;
 		if (bytes_left >= DISK_SECTOR_SIZE) {
@@ -87,6 +91,9 @@ fat_close (void) {
 	uint8_t *bounce = calloc (1, DISK_SECTOR_SIZE);
 	if (bounce == NULL)
 		PANIC ("FAT close failed");
+
+	// bounce를 FAT_BOOT_SECTOR에 옮겨쓴다. 
+	// 이러면 FAT_BOOT_SECTOR는 bs와 동일한 것이라는 것을 알 수 있다.
 	memcpy (bounce, &fat_fs->bs, sizeof (fat_fs->bs));
 	disk_write (filesys_disk, FAT_BOOT_SECTOR, bounce);
 	free (bounce);
@@ -117,15 +124,18 @@ fat_close (void) {
 void
 fat_create (void) {
 	// Create FAT boot
-	fat_boot_create ();
-	fat_fs_init ();
+	fat_boot_create (); // fat_fs->bs를 만들어준다.
+	fat_fs_init (); // fat_fs를 만들어준다.
 
 	// Create FAT table
+	// fat_fs->fat : cluster 크기 * cluster 갯수만큼의 메모리를 할당
+	// 즉, fat에는 디스크의 cluster의 주소 정보가 모두 들어간다는 것
 	fat_fs->fat = calloc (fat_fs->fat_length, sizeof (cluster_t));
 	if (fat_fs->fat == NULL)
 		PANIC ("FAT creation failed");
 
 	// Set up ROOT_DIR_CLST
+	// ROOT_DIR_CLUSTER : 1
 	fat_put (ROOT_DIR_CLUSTER, EOChain);
 
 	// Fill up ROOT_DIR_CLUSTER region with 0
@@ -142,12 +152,14 @@ fat_boot_create (void) {
 	    (disk_size (filesys_disk) - 1)
 	    / (DISK_SECTOR_SIZE / sizeof (cluster_t) * SECTORS_PER_CLUSTER + 1) + 1;
 	fat_fs->bs = (struct fat_boot){
-	    .magic = FAT_MAGIC,
-	    .sectors_per_cluster = SECTORS_PER_CLUSTER,
+	    .magic = FAT_MAGIC, // 오염 판단 기준
+	    .sectors_per_cluster = SECTORS_PER_CLUSTER, // 1
 	    .total_sectors = disk_size (filesys_disk),
+		// 0번 섹터는 디스크의 정보를 가지고 있음.
+		// 1번 섹터는 root_dir를 말하는데 여기서부터 시작해도 되나..?
 	    .fat_start = 1,
-	    .fat_sectors = fat_sectors,
-	    .root_dir_cluster = ROOT_DIR_CLUSTER,
+	    .fat_sectors = fat_sectors, // Size of FAT in sectors
+	    .root_dir_cluster = ROOT_DIR_CLUSTER, // 1
 	};
 }
 
